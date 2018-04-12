7d1aaa5e7a60a0ee24323eeb6f22962ca7c22e7f
yuansilin <silin@acuo.com>
2018-04-12 13:34:20 +0800

    OW-990, refactored the code due to parameters changes

---
+++ src/test/java/learning/simpleTest.java
@@ -0,0 +1,14 @@
+package learning;
+
+import lpsolve.LpSolve;
+import lpsolve.VersionInfo;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class simpleTest {
+    @Test public void aString() throws Exception {
+        String str = "string";
+        assertEquals(str, "string");
+    }
+}
