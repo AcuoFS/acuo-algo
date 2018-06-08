e768404dc28e89b790d7c1ffe856390a999a971a
Hicham Medkouri <hicham.medkouri@gmail.com>
2018-06-08 09:45:16 +0100

    OW-483 added unit test for LpSolve

--- src/test/groovy/com/acuo/algo/javaLpSolve/CallLpSolveSpec.groovy
+++ src/test/groovy/com/acuo/algo/javaLpSolve/CallLpSolveSpec.groovy
@@ -1,26 +1,42 @@
 package com.acuo.algo.javaLpSolve
 
+import com.acuo.algo.NativeUtils
 import com.acuo.algo.RenjinEval
-import com.acuo.algo.Renjin
 import org.renjin.sexp.SEXP
 import spock.lang.Specification
 
-import static com.acuo.algo.RenjinMatchers.closeTo
-
 import static org.hamcrest.Matchers.equalTo
-import static spock.util.matcher.HamcrestSupport.expect
 import static spock.util.matcher.HamcrestSupport.that
 
 class CallLpSolveSpec extends Specification implements RenjinEval {
-    void setup() {
-        eval("library(stats)")
-        eval("library(lpSolveAPI)")
-        eval("library('com.acuo.collateral.acuo-algo')")
-    }
-    void "CallLpSolve calculates the Reserved Liquidity Ratio for available assets pool"() {
-        expect:
-        eval('lpModel <- LpSolve$makeLp(0, 2)')
-        that eval('2'), equalTo(2d as SEXP)
-    }
+  static {
+    NativeUtils.load("lpsolve55", "lpsolve55j")
+  }
+
+  void setup() {
+    eval("library(stats)")
+    eval("library('com.acuo.collateral.acuo-algo')")
+    eval('import(lpsolve.LpSolve)')
+  }
+
+  void "CallLpSolve calculates the Reserved Liquidity Ratio for available assets pool"() {
+    given:
+    eval('lpModel <- LpSolve$makeLp(0L, 4L)')
+    eval('lpModel$strAddConstraint("3 2 2 1", 1L, 4)')
+    eval('lpModel$strAddConstraint("0 4 3 1", 2L, 3)')
+    eval('lpModel$strSetObjFn("2 3 -2 3")')
+    eval('lpModel$solve()')
+
+    when:
+    SEXP obj = (SEXP) eval('obj <- lpModel$getObjective()')
+    SEXP res = (SEXP) eval('res <- lpModel$getPtrVariables()')
+
+    then:
+    that obj, equalTo(c(-4.0d) as SEXP)
+    that res, equalTo(c(0,0,2,0) as SEXP)
+
+    cleanup:
+    eval('lpModel$deleteLp()')
+  }
 
 }
