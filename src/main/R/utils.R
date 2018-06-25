7fd74755d3e29ed9fc470635d4aa060b07fe209a
yuansilin <silin@acuo.com>
2018-06-25 16:21:25 +0800

    OW-987, relocated functions based on their usage

--- src/main/R/utils.R
+++ src/main/R/utils.R
@@ -5,9 +5,26 @@ CalculateIntegralUnit <- function(amount,valuePerUnit,discount){
   return(intUnit)
 }
 
-RemoveRowsInAvailAsset <- function(availAsset_df,rmIdx_vec){
-  if(length(rmIdx_vec)>0){
-    availAsset_df <- availAsset_df[-rmIdx_vec,]
+UsedQtyFromResultList <- function(result_list,resource_vec,callId_vec){
+  #### minUnitQuantity of resources used for allocation
+  quantityUsed_vec <- rep(0,length(resource_vec))
+  callNum <- length(callId_vec)
+  resourceNum <- length(resource_vec)
+  print('resource_vec'); print(resource_vec)
+  for(i in 1:callNum){
+    callId <- callId_vec[i]
+    alloc_df <- result_list[[callId]]
+    print('alloc_df'); print(alloc_df)
+    resourceTemp_vec <- PasteResource(alloc_df$Asset,alloc_df$CustodianAccount)
+    print('resourceTemp_vec'); print(resourceTemp_vec)
+    idxInRes_vec <- na.omit(match(resourceTemp_vec,resource_vec))
+    print('idxInRes_vec'); print(idxInRes_vec)
+    if(length(idxInRes_vec)!=0){
+      idxInAlloc <- match(resource_vec[idxInRes_vec],resourceTemp_vec)
+      print('idxInAlloc'); print(idxInAlloc)
+
+      quantityUsed_vec[idxInRes_vec] <- quantityUsed_vec[idxInRes_vec]+alloc_df$Quantity[idxInAlloc]
+    }
   }
-  return(availAsset_df)
+  return(quantityUsed_vec)
 }
