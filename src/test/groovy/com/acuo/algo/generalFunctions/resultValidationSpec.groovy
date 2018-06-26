ab16a0a7d84fa3b88f70276e3b3e4adee8564cdf
yuansilin <silin@acuo.com>
2018-06-26 17:50:15 +0800

    OW-987, added a unit test for CheckSolverResult in resultValidation.R

--- src/test/groovy/com/acuo/algo/generalFunctions/resultValidationSpec.groovy
+++ src/test/groovy/com/acuo/algo/generalFunctions/resultValidationSpec.groovy
@@ -15,25 +15,6 @@ class resultValidationSpec extends Specification implements RenjinEval {
       eval("library(stats)")
       eval("library('com.acuo.collateral.acuo-algo')")
   }
-  void "AdjustResultVec adjusts the solver solution by rounding"() {
-      when:
-      eval('solution_vec <- c(1000.8,2000.1,4000,1,0)')
-      eval('varName_vec <- c(\'x1\',\'x2\',\'x3\',\'x4\',\'x5\')')
-      eval('varNum <- 3')
-      eval('callAmount_vec <- c(3000,4000,3000)')
-      eval('minUnitQuantity_vec <- c(5000,6000,5000)')
-      eval('minUnitValue_vec <- c(1,1,1)')
-      eval('fCon4_mat <- matrix(c(1,0,1,-5001,0,' +
-                                    '0,1,0,0,-6001,' +
-                                    '1,0,1,-1,0,' +
-                                    '0,1,0,0,-1), nrow = 4,byrow = T)')
-
-      eval('adjustedSolution_vec <- AdjustResultVec(solution_vec,varNum,varName_vec,fCon4_mat,\n' +
-              'callAmount_vec,minUnitQuantity_vec,minUnitValue_vec)')
-      eval('print(adjustedSolution_vec)')
-      then:
-      that eval('adjustedSolution_vec'), equalTo(c(1001,2001,4000,1,1) as SEXP)
-  }
 
   void "CheckLowerBound checks decision variables' lower bound"() {
     when:// result violates the constraints
@@ -195,20 +176,30 @@ class resultValidationSpec extends Specification implements RenjinEval {
     that eval('x'), equalTo(eval('NULL') as SEXP)
   }
 
-  void "CheckResultVec checks the allocation result and adjusts it for over used resource quantity"() {
+  void "CheckSolverResult validates allocation result by certain constraints"() {
     when:
-    eval('result_mat <- matrix(c(1000,2001,0,4000), nrow = 2,byrow = T)')
-    eval('quantityTotal_vec <- c(5000,6000)')
-    eval('callId_vec <- c(\'mc1\',\'mc2\')')
-    eval('callAmount_vec <- c(3000,4000)')
-    eval('minUnitValue_mat <- matrix(c(1,1,1,1),nrow = 2, byrow = T)')
-    eval('haircut_mat <- matrix(c(0,0,0,0),nrow = 2, byrow = T)')
-    eval('eli_mat <- matrix(c(1,1,1,1),nrow = 2, byrow = T)')
+    eval('solution_vec <- c(1500,2000,4000,1,1)')
+    eval('result_mat <- matrix(c(1500,2000,4000,0),nrow=2,byrow=T)')
+    eval('varName_vec <- c(\'ms1___mc1___EUR---ca2\',\'ms1___mc1___USD---ca1\',\'ms1___mc2___EUR---ca2\',' +
+      '\'ms1___dummy___EUR---ca2\',\'ms1___dummy___USD---ca1\')')
+    eval('callInfo_df <- data.frame(\n' +
+      'id=c(\'mc1\',\'mc2\'),\n' +
+      'marginStatement=c(\'ms1\',\'ms1\'),\n' +
+      'marginType=c(\'Initial\',\'Variation\'))')
+    eval('resourceQty_vec <- c(6000,6000)')
+    eval('minUnitValue_mat <- matrix(c(1,1,1,1),nrow=2,byrow=T)')
+    eval('haircut_mat <- matrix(c(0.2,0.1,0.2,0.1),nrow=2,byrow=T)')
+    eval('lpLowerBound_vec <- c(1000,1000,1000,0,0)')
+    eval('lpUpperBound_vec <- c(6000,4000,6000,1,1)')
+    eval('operLimitMs <- 2')
+    eval('fungible <- FALSE')
 
-    eval('checkResult <- CheckResultVec(result_mat,quantityTotal_vec,callId_vec,callAmount_vec,minUnitValue_mat,haircut_mat,eli_mat)')
-    eval('print(checkResult)')
+    eval('x <- tryCatch(CheckSolverResult(solution_vec,result_mat,varName_vec,callInfo_df,resourceQty_vec,minUnitValue_mat,haircut_mat,\n' +
+      '                              lpLowerBound_vec,lpUpperBound_vec,operLimitMs,fungible),' +
+      'error = function(e){' +
+      'print(e$message)' +
+      '})')
     then:
-    that eval('checkResult$result_mat[1,]'), equalTo(c(1000,2000) as SEXP)
-    that eval('checkResult$result_mat[2,]'), equalTo(c(0,4000) as SEXP)
+    that eval('x'), equalTo(eval('NULL') as SEXP)
   }
 }
